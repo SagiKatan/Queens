0e82ec5b005736fdc2e49abeb960771896175d0a
Sagi Katan <sagikatan@gmail.com>
2025-03-04 23:06:20 +0200

    add main

---
+++ Level1.py
@@ -0,0 +1,10 @@
+import pygame
+matrix = [[0 for j in range(7)] for i in range(7)]
+solution = [[0 for j in range(7)] for i in range(7)]
+solution[0][0] = 1
+solution[1][5] = 1
+solution[2][3] = 1
+solution[3][1] = 1
+solution[4][4] = 1
+solution[5][2] = 1
+solution[6][6] = 1
